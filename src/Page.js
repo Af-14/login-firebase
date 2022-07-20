d8b3209bbdf4f8ca329625fac4ac486bb849a636
afifi <afifi@besquare.com.my>
2022-07-21 02:16:20 +0800

    new

---
+++ src/Page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Page = (handleLogout) => {
+  return (
+    <section className="page">
+      <nav>
+        <h2>Welcome</h2>
+        <button onClick={handleLogout}>Logout</button>
+      </nav>
+    </section>
+  );
+};
+
+export default Page;
