5ce5711d43546e8f75ed5c40823e3ba742eb4e83
afifi <afifi@besquare.com.my>
2022-07-21 14:11:48 +0800

    firebase

--- src/Page.js
+++ src/Page.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Page = (handleLogout) => {
+const Page = ({ handleLogout }) => {
   return (
     <section className="page">
       <nav>
         <h2>Welcome</h2>
-        <button onClick={handleLogout}>Logout</button>
+        <button onClick={() => handleLogout()}>Logout</button>
       </nav>
     </section>
   );
